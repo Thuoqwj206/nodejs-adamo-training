a5e39ed42f029a73ce5797b3361dc01c5dc30ffc
thuongpv <88428890+Thuoqwj206@users.noreply.github.com>
2023-12-28 09:57:29 +0700

    resolved

--- js-advance/ex-18.ts
+++ js-advance/ex-18.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const countNumber = (n: number) => n.toString().length
-=======
-const countNumber = (n: number) => n.toString().split('').length
->>>>>>> a37dc43880572342c400b9f9fd1852f5027a1c2b
 
 
 console.log(countNumber(3423434324234))
--- js-advance/ex-23.ts
+++ js-advance/ex-23.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const reverseNumber = (x: number): number => x.toString().split('').reverse().join('') as unknown as number
-=======
-const reverseNumber = (x: number) => x.toString().split('').reverse().join('')
->>>>>>> a37dc43880572342c400b9f9fd1852f5027a1c2b
 // let reverseNum = 0;
 // while (x != 0) {
 //     let digit = x % 10;
