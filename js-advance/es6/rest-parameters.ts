1f5d80132d866e904c6e2fefd2529c4cec70a385
thuongpv <88428890+Thuoqwj206@users.noreply.github.com>
2023-12-28 08:03:27 +0700

    refix after review

--- js-advance/es6/rest-parameters.ts
+++ js-advance/es6/rest-parameters.ts
@@ -1,11 +1,6 @@
-const sumAll = (...rests:number[]) => {
-    let sum = 0;
-    rests.map(
-        (rest) => {sum+=rest}
-    )
-    return sum;
-}
+const sumAll = (...rests: number[]) => rests.reduce((sum, rest) => sum + rest)
 
-console.log(sumAll(1,2,3))
-console.log(sumAll(1,2,3,4,5))
-console.log(sumAll(1,2,3,4,5,5,6,7))
+
+console.log(sumAll(1, 2, 3))
+console.log(sumAll(1, 2, 3, 4, 5))
+console.log(sumAll(1, 2, 3, 4, 5, 5, 6, 7))
--- js-advance/ex-14.ts
+++ js-advance/ex-14.ts
@@ -1,13 +1,9 @@
-let array1 = [1, 2, 8, 4, 1, 3, 1, 8, 3];
+const array1 = [1, 2, 8, 4, 1, 3, 1, 8, 3];
 
 const counter = {};
 
 array1.map(ele => {
-    if (counter[ele]) {
-        counter[ele] += 1;
-    } else {
-        counter[ele] = 1;
-    }
+    counter[ele] ? counter[ele] += 1 : counter[ele] = 1
 });
 
 console.log(counter)
